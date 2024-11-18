7312c06a8c598ef1853f9361f890405c61e083a6
mrpota2 <adrian.milvang@gmail.com>
2024-11-18 13:52:32 +0100

    lets pretend its error handling

--- displayControll.py
+++ displayControll.py
@@ -19,24 +19,29 @@ def find_pico_com_port():
 
 def getPot(board):
     while True:
-        if board.in_waiting > 0:
-            line = board.readline().decode("utf-8").strip()
-            print(line)
-            pot, sw = line.split(" ")
-            return int(round(float(pot))), int(sw)
+        try:
+            if board.in_waiting > 0:
+                line = board.readline().decode("utf-8").strip()
+                pot, sw = line.split(" ")
+                return int(round(float(pot))), int(sw)
+        except Exception as e:
+            raise e
+        sleep(0.1)
 
 
 def changeBrightness(pot, sw):
-    monitor = mc.get_monitors()[sw]
-    with monitor:
-        print("Changing brightness of monitor", sw)
-        prev_luminance = monitor.get_luminance()
-        luminance = prev_luminance + pot
-        if luminance > 100:
-            luminance = 100
-        elif luminance < 0:
-            luminance = 0
-        monitor.set_luminance(luminance)
+    try:
+        monitor = mc.get_monitors()[sw]
+        with monitor:
+            prev_luminance = monitor.get_luminance()
+            luminance = prev_luminance + pot
+            if luminance > 100:
+                luminance = 100
+            elif luminance < 0:
+                luminance = 0
+            monitor.set_luminance(luminance)
+    except mc.VCPError as e:
+        return
 
 
 def main():
@@ -52,9 +57,10 @@ def main():
             try:
                 pot, sw = getPot(board)
                 changeBrightness(-pot, sw)
-            except Exception as e:
-                print("Error changing brightness:", e)
+            except s.SerialException as e:
                 break
+            except Exception as e:
+                continue
 
 
 main()
